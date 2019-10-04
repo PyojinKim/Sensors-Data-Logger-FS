ff6cba18fc9e86f813958ff23ac8886a51389ee3
pjinkim-PC\pjinkim <pjinkim1215@gmail.com>
2019-10-04 14:21:58 +0900

    update datasets.

--- RoNIN_Alignment_core/setupParams_Smartphone_Dataset.m
+++ RoNIN_Alignment_core/setupParams_Smartphone_Dataset.m
@@ -10,6 +10,18 @@ switch( expCase )
         datasetPath = 'G:/Smartphone_Dataset/1_Lifelong_Learning/Samsung_Galaxy_S9/20191001061017R_pjinkim';
         
     case 3
-        datasetPath = 'G:/Smartphone_Dataset/1_Lifelong_Learning/Google_Pixel_2_XL/20190930035631R_pjinkim';
+        datasetPath = 'G:/Smartphone_Dataset/1_Lifelong_Learning/Samsung_Galaxy_S9/20191002091948R_pjinkim';
+        
+    case 4
+        datasetPath = 'G:/Smartphone_Dataset/1_Lifelong_Learning/Samsung_Galaxy_S9/20191003052013R_pjinkim';
+        
+    case 5
+        datasetPath = 'G:/Smartphone_Dataset/1_Lifelong_Learning/Samsung_Galaxy_S9/20191003062113R_pjinkim';
+        
+    case 6
+        datasetPath = 'G:/Smartphone_Dataset/1_Lifelong_Learning/Apple_iPhone_XS/20191003_1';
+        
+    case 7
+        datasetPath = 'G:/Smartphone_Dataset/1_Lifelong_Learning/Apple_iPhone_XS/20191003_2';
         
 end
