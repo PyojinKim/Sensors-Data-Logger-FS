ce06f43e91490bb61af1add435afe54ce3917982
pjinkim <pjinkim1215@gmail.com>
2020-01-13 17:23:01 -0800

    first commit of WiFi SfM codes in this repository.

---
+++ WiFi_SfM_core/queryWiFiRSSI.m
@@ -0,0 +1,46 @@
+function [queryLocation, maxRewardIndex, rewardResult] = queryWiFiRSSI(queryRSSI, wifiFingerprintDatabase)
+
+% compare query RSSI to WiFi RSSI database
+numLabeledWiFiScan = size(wifiFingerprintDatabase,2);
+rewardResult = zeros(1,numLabeledWiFiScan);
+for k = 1:numLabeledWiFiScan
+    
+    % calculate reward function based on the RSSI vectors
+    databaseRSSI = wifiFingerprintDatabase(k).RSSI;
+    rewardResult(k) = computeRewardMetric(queryRSSI, databaseRSSI);
+end
+
+
+% K-nearest neighbour (kNN) based WiFi positioning
+numNeighbors = 4;
+[~,maxRewardIndex] = sort(rewardResult, 'descend');
+maxRewardIndex = maxRewardIndex(1:numNeighbors);
+queryLocation = mean([wifiFingerprintDatabase(maxRewardIndex).trueLocation],2);
+
+
+end
+
+
+%% distance metric (deprecated)
+
+% % compare query RSSI to WiFi RSSI database
+% numLabeledWiFiScan = size(wifiFingerprintDatabase,2);
+% distanceResult = 50 * ones(1,numLabeledWiFiScan);
+% for k = 1:numLabeledWiFiScan
+%
+%     % compute RSSI distance metric
+%     databaseRSSI = wifiFingerprintDatabase(k).RSSI;
+%     distance = computeDistanceMetric(queryRSSI, databaseRSSI, 'L1');
+%     distanceResult(k) = distance;
+% end
+%
+%
+% % find top five index
+% distanceResult(queryIndex) = 100;
+% [~,firstIndex] = min(distanceResult);
+%
+%
+% figure;
+% plot(distanceResult);
+% xlabel('WiFi Scan Location Index'); ylabel('Distance Metric (L1)');
+%
