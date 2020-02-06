d430490eb0b7a5a303e4b527d244a9a79d2c9afd
pyojink <pyojink@cs-vml-45.cs.sfu.ca>
2020-02-05 18:21:46 -0800

    add network code

--- Siamese_Network_core/main_script_MATLAB_example.m
+++ Siamese_Network_core/main_script_MATLAB_example.m
@@ -20,18 +20,16 @@ end
 disp("Training data downloaded.")
 
 
-%
+% load the Omniglot training dataset
 imds = omniglotImageDatastore(trainingSetFolder);
 
 
-%
+% display a random selection of the sample images
 idxs = randperm(numel(imds.Files),8);
-
-figure;
 for i = 1:numel(idxs)
     subplot(4,2,i)
     imshow(readimage(imds,idxs(i)))
-    title(string(imds.Labels(idxs(i))),"Interpreter","none");
+    title(imds.Labels(idxs(i)), "Interpreter","none");
 end
 
 
@@ -73,20 +71,88 @@ layers = [
 lgraph = layerGraph(layers);
 
 
+% convert the layer graph to a dlnetwork object
+dlnet = dlnetwork(lgraph);
+
+fcWeights = dlarray(0.01*randn(1,4096));
+fcBias = dlarray(0.01*randn(1,1));
+fcParams = struct("FcWeights",fcWeights,"FcBias",fcBias);
 
 
+%% Specify Training Options
+
+numIterations = 10000;
+miniBatchSize = 180;
+
+learningRate = 6e-5;
+trailingAvgSubnet = [];
+trailingAvgSqSubnet = [];
+trailingAvgParams = [];
+trailingAvgSqParams = [];
+gradDecay = 0.9;
+gradDecaySq = 0.99;
+
+executionEnvironment = "auto";
+plots = "training-progress";
+
+plotRatio = 16/9;
+
+if plots == "training-progress"
+    trainingPlot = figure;
+    trainingPlot.Position(3) = plotRatio*trainingPlot.Position(4);
+    trainingPlot.Visible = 'on';
+    
+    trainingPlotAxes = gca;
+    
+    lineLossTrain = animatedline(trainingPlotAxes);
+    xlabel(trainingPlotAxes,"Iteration")
+    ylabel(trainingPlotAxes,"Loss")
+    title(trainingPlotAxes,"Loss During Training")
+end
 
 
+%% Train Model
 
-
-
-
-
-
-
-
-
-
+% Loop over mini-batches.
+for iteration = 1:numIterations
+    
+    % Extract mini-batch of image pairs and pair labels
+    [X1,X2,pairLabels] = getSiameseBatch(imds,miniBatchSize);
+    
+    % Convert mini-batch of data to dlarray. Specify the dimension labels
+    % 'SSCB' (spatial, spatial, channel, batch) for image data
+    dlX1 = dlarray(single(X1),'SSCB');
+    dlX2 = dlarray(single(X2),'SSCB');
+    
+    % If training on a GPU, then convert data to gpuArray.
+    if (executionEnvironment == "auto" && canUseGPU) || executionEnvironment == "gpu"
+        dlX1 = gpuArray(dlX1);
+        dlX2 = gpuArray(dlX2);
+    end
+    
+    % Evaluate the model gradients and the generator state using
+    % dlfeval and the modelGradients function listed at the end of the
+    % example.
+    [gradientsSubnet, gradientsParams,loss] = dlfeval(@modelGradients,dlnet,fcParams,dlX1,dlX2,pairLabels);
+    lossValue = double(gather(extractdata(loss)));
+    
+    % Update the Siamese subnetwork parameters.
+    [dlnet,trailingAvgSubnet,trailingAvgSqSubnet] = ...
+        adamupdate(dlnet,gradientsSubnet, ...
+        trailingAvgSubnet,trailingAvgSqSubnet,iteration,learningRate,gradDecay,gradDecaySq);
+    
+    % Update the fullyconnect parameters.
+    [fcParams,trailingAvgParams,trailingAvgSqParams] = ...
+        adamupdate(fcParams,gradientsParams, ...
+        trailingAvgParams,trailingAvgSqParams,iteration,learningRate,gradDecay,gradDecaySq);
+    
+    % Update the training loss progress plot.
+    if plots == "training-progress"
+        addpoints(lineLossTrain,iteration,lossValue);
+    end
+    drawnow;
+    iteration
+end
 
 
 
