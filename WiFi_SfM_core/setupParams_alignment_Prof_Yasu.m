a9352e38fe5a1ab8248a1fc8c051b56010dd5e27
pjinkim <pjinkim1215@gmail.com>
2020-03-04 16:43:55 -0800

    add all Prof. Yasu's dataset infomation.

--- WiFi_SfM_core/setupParams_alignment_Prof_Yasu.m
+++ WiFi_SfM_core/setupParams_alignment_Prof_Yasu.m
@@ -3,6 +3,70 @@
 switch( expCase )
     
     case 1
+        datasetDirectory = 'G:/Smartphone_Dataset/4_WiFi_SfM/Prof_Yasu/20191211120034R_WiFi_SfM';
+        roninInterval = 200;          % 1 Hz
+        roninYawRotation = 0;       % degree
+        
+        % valid RoNIN index after removing RoNIN stationary motion
+        validRoninIndex = 1:5768;
+        
+    case 2
+        datasetDirectory = 'G:/Smartphone_Dataset/4_WiFi_SfM/Prof_Yasu/20191212091905R_WiFi_SfM';
+        roninInterval = 200;          % 1 Hz
+        roninYawRotation = 0;       % degree
+        
+        % valid RoNIN index after removing RoNIN stationary motion
+        validRoninIndex = 1:7796;
+        
+    case 3
+        datasetDirectory = 'G:/Smartphone_Dataset/4_WiFi_SfM/Prof_Yasu/20191213092310R_WiFi_SfM';
+        roninInterval = 200;          % 1 Hz
+        roninYawRotation = 0;       % degree
+        
+        % valid RoNIN index after removing RoNIN stationary motion
+        validRoninIndex = 1:6113;
+        
+    case 4
+        datasetDirectory = 'G:/Smartphone_Dataset/4_WiFi_SfM/Prof_Yasu/20191216102716R_WiFi_SfM';
+        roninInterval = 200;          % 1 Hz
+        roninYawRotation = 0;       % degree
+        
+        % valid RoNIN index after removing RoNIN stationary motion
+        validRoninIndex = 1:2963;
+        
+    case 5
+        datasetDirectory = 'G:/Smartphone_Dataset/4_WiFi_SfM/Prof_Yasu/20191217092830R_WiFi_SfM';
+        roninInterval = 200;          % 1 Hz
+        roninYawRotation = 0;       % degree
+        
+        % valid RoNIN index after removing RoNIN stationary motion
+        validRoninIndex = 1:3210;
+        
+    case 6
+        datasetDirectory = 'G:/Smartphone_Dataset/4_WiFi_SfM/Prof_Yasu/20191218101306R_WiFi_SfM';
+        roninInterval = 200;          % 1 Hz
+        roninYawRotation = 0;       % degree
+        
+        % valid RoNIN index after removing RoNIN stationary motion
+        validRoninIndex = 1:10594;
+        
+    case 7
+        datasetDirectory = 'G:/Smartphone_Dataset/4_WiFi_SfM/Prof_Yasu/20191219100730R_WiFi_SfM';
+        roninInterval = 200;          % 1 Hz
+        roninYawRotation = 0;       % degree
+        
+        % valid RoNIN index after removing RoNIN stationary motion
+        validRoninIndex = 1:3842;
+        
+    case 8
+        datasetDirectory = 'G:/Smartphone_Dataset/4_WiFi_SfM/Prof_Yasu/20191220110037R_WiFi_SfM';
+        roninInterval = 200;          % 1 Hz
+        roninYawRotation = 0;       % degree
+        
+        % valid RoNIN index after removing RoNIN stationary motion
+        validRoninIndex = 1:4426;
+        
+    case 9
         datasetDirectory = 'G:/Smartphone_Dataset/4_WiFi_SfM/Prof_Yasu/20200106014640R_WiFi_SfM';
         roninInterval = 200;          % 1 Hz
         roninYawRotation = 0;       % degree
@@ -10,7 +74,7 @@ switch( expCase )
         % valid RoNIN index after removing RoNIN stationary motion
         validRoninIndex = 1:2033;
         
-    case 2
+    case 10
         datasetDirectory = 'G:/Smartphone_Dataset/4_WiFi_SfM/Prof_Yasu/20200107091503R_WiFi_SfM';
         roninInterval = 200;          % 1 Hz
         roninYawRotation = 0;       % degree
@@ -18,7 +82,7 @@ switch( expCase )
         % valid RoNIN index after removing RoNIN stationary motion
         validRoninIndex = 1:9646;
         
-    case 3
+    case 11
         datasetDirectory = 'G:/Smartphone_Dataset/4_WiFi_SfM/Prof_Yasu/20200108112257R_WiFi_SfM';
         roninInterval = 200;          % 1 Hz
         roninYawRotation = 0;       % degree
@@ -26,7 +90,7 @@ switch( expCase )
         % valid RoNIN index after removing RoNIN stationary motion
         validRoninIndex = 1:10318;
         
-    case 4
+    case 12
         datasetDirectory = 'G:/Smartphone_Dataset/4_WiFi_SfM/Prof_Yasu/20200109090901R_WiFi_SfM';
         roninInterval = 200;          % 1 Hz
         roninYawRotation = 0;       % degree
@@ -34,7 +98,55 @@ switch( expCase )
         % valid RoNIN index after removing RoNIN stationary motion
         validRoninIndex = 1:2711;
         
-    case 5
+    case 13
+        datasetDirectory = 'G:/Smartphone_Dataset/4_WiFi_SfM/Prof_Yasu/20200110095525R_WiFi_SfM';
+        roninInterval = 200;          % 1 Hz
+        roninYawRotation = 0;       % degree
+        
+        % valid RoNIN index after removing RoNIN stationary motion
+        validRoninIndex = 1:8098;
+        
+    case 14
+        datasetDirectory = 'G:/Smartphone_Dataset/4_WiFi_SfM/Prof_Yasu/20200113093600R_WiFi_SfM';
+        roninInterval = 200;          % 1 Hz
+        roninYawRotation = 0;       % degree
+        
+        % valid RoNIN index after removing RoNIN stationary motion
+        validRoninIndex = 1:5335;
+        
+    case 15
+        datasetDirectory = 'G:/Smartphone_Dataset/4_WiFi_SfM/Prof_Yasu/20200114104643R_WiFi_SfM';
+        roninInterval = 200;          % 1 Hz
+        roninYawRotation = 0;       % degree
+        
+        % valid RoNIN index after removing RoNIN stationary motion
+        validRoninIndex = 1:5514;
+        
+    case 16
+        datasetDirectory = 'G:/Smartphone_Dataset/4_WiFi_SfM/Prof_Yasu/20200116091923R_WiFi_SfM';
+        roninInterval = 200;          % 1 Hz
+        roninYawRotation = 0;       % degree
+        
+        % valid RoNIN index after removing RoNIN stationary motion
+        validRoninIndex = 1:9824;
+        
+    case 17
+        datasetDirectory = 'G:/Smartphone_Dataset/4_WiFi_SfM/Prof_Yasu/20200117094121R_WiFi_SfM';
+        roninInterval = 200;          % 1 Hz
+        roninYawRotation = 0;       % degree
+        
+        % valid RoNIN index after removing RoNIN stationary motion
+        validRoninIndex = 1:9037;
+        
+    case 18
+        datasetDirectory = 'G:/Smartphone_Dataset/4_WiFi_SfM/Prof_Yasu/20200120113658R_WiFi_SfM';
+        roninInterval = 200;          % 1 Hz
+        roninYawRotation = 0;       % degree
+        
+        % valid RoNIN index after removing RoNIN stationary motion
+        validRoninIndex = 1:3840;
+        
+    case 19
         datasetDirectory = 'G:/Smartphone_Dataset/4_WiFi_SfM/Prof_Yasu/20200121091935R_WiFi_SfM';
         roninInterval = 200;          % 1 Hz
         roninYawRotation = 0;       % degree
@@ -42,7 +154,7 @@ switch( expCase )
         % valid RoNIN index after removing RoNIN stationary motion
         validRoninIndex = 1:11638;
         
-    case 6
+    case 20
         datasetDirectory = 'G:/Smartphone_Dataset/4_WiFi_SfM/Prof_Yasu/20200123091630R_WiFi_SfM';
         roninInterval = 200;          % 1 Hz
         roninYawRotation = 0;       % degree
@@ -50,7 +162,7 @@ switch( expCase )
         % valid RoNIN index after removing RoNIN stationary motion
         validRoninIndex = 1:8955;
         
-    case 7
+    case 21
         datasetDirectory = 'G:/Smartphone_Dataset/4_WiFi_SfM/Prof_Yasu/20200124092153R_WiFi_SfM';
         roninInterval = 200;          % 1 Hz
         roninYawRotation = 0;       % degree
@@ -58,7 +170,7 @@ switch( expCase )
         % valid RoNIN index after removing RoNIN stationary motion
         validRoninIndex = 1:13336;
         
-    case 8
+    case 22
         datasetDirectory = 'G:/Smartphone_Dataset/4_WiFi_SfM/Prof_Yasu/20200128105904R_WiFi_SfM';
         roninInterval = 200;          % 1 Hz
         roninYawRotation = 0;       % degree
@@ -66,7 +178,7 @@ switch( expCase )
         % valid RoNIN index after removing RoNIN stationary motion
         validRoninIndex = 1:3640;
         
-    case 9
+    case 23
         datasetDirectory = 'G:/Smartphone_Dataset/4_WiFi_SfM/Prof_Yasu/20200129092100R_WiFi_SfM';
         roninInterval = 200;          % 1 Hz
         roninYawRotation = 0;       % degree
@@ -74,7 +186,7 @@ switch( expCase )
         % valid RoNIN index after removing RoNIN stationary motion
         validRoninIndex = 1:7277;
         
-    case 10
+    case 24
         datasetDirectory = 'G:/Smartphone_Dataset/4_WiFi_SfM/Prof_Yasu/20200130091304R_WiFi_SfM';
         roninInterval = 200;          % 1 Hz
         roninYawRotation = 0;       % degree
@@ -82,7 +194,7 @@ switch( expCase )
         % valid RoNIN index after removing RoNIN stationary motion
         validRoninIndex = 1:7577;
         
-    case 11
+    case 25
         datasetDirectory = 'G:/Smartphone_Dataset/4_WiFi_SfM/Prof_Yasu/20200131092145R_WiFi_SfM';
         roninInterval = 200;          % 1 Hz
         roninYawRotation = 0;       % degree
@@ -90,7 +202,7 @@ switch( expCase )
         % valid RoNIN index after removing RoNIN stationary motion
         validRoninIndex = 1:4873;
         
-    case 12
+    case 26
         datasetDirectory = 'G:/Smartphone_Dataset/4_WiFi_SfM/Prof_Yasu/20200205092720R_WiFi_SfM';
         roninInterval = 200;          % 1 Hz
         roninYawRotation = 0;       % degree
@@ -98,7 +210,7 @@ switch( expCase )
         % valid RoNIN index after removing RoNIN stationary motion
         validRoninIndex = 1:4856;
         
-    case 13
+    case 27
         datasetDirectory = 'G:/Smartphone_Dataset/4_WiFi_SfM/Prof_Yasu/20200206091600R_WiFi_SfM';
         roninInterval = 200;          % 1 Hz
         roninYawRotation = 0;       % degree
@@ -106,7 +218,7 @@ switch( expCase )
         % valid RoNIN index after removing RoNIN stationary motion
         validRoninIndex = 1:11132;
         
-    case 14
+    case 28
         datasetDirectory = 'G:/Smartphone_Dataset/4_WiFi_SfM/Prof_Yasu/20200207101556R_WiFi_SfM';
         roninInterval = 200;          % 1 Hz
         roninYawRotation = 0;       % degree
